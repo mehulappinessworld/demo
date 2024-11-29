218a9af2089f57c3df1c2eed7276e5113accd6b6
kalathiya.mehul <kalathiyamehul13899@gmail.com>
2024-11-29 09:43:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 require('dotenv').config()
 const apiRoutes = require('./src/routes/api')
-const bodyParser = require('body-parser')
 const app = express()
-app.use(bodyParser.json())
+app.use(express.json())
 app.use('/api', apiRoutes)
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
